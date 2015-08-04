310b2191d1e186bf59ec16f762d4a732b15e98db
Andrew9Tech <Andrew9Tech@users.noreply.github.com>
2015-08-04 12:55:23 +0800

    Create 字符串合并处理.cpp

---
+++ 字符串合并处理.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+void Solution(char * X, char * Y)
+{
+
+}
+
+void main()
+{
+	char str1[100]; char str2[100];
+	cin >> str1;
+	cin >> str2;
+
+	Solution(str1, str2);
+
+	/* 后缀数组方法 */
+
+	//LCS_suffix(X, strlen(X), Y, strlen(Y));
+}
